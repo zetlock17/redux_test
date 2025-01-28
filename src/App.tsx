75a0ff8091858e265d8dc511e834eeb8627c8d54
zetlock17 <95153376+zetlock17@users.noreply.github.com>
2025-01-28 23:51:00 +1000

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import './styles/components/header.scss';
 
 const App: React.FC = () => {
     return (
-        <div>
+        <div className='header'>
             <TodoList /> {/* Рендерим компонент списка задач */}
         </div>
     );
